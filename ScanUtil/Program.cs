3aadd991257b01155780a44173324c6b92520d71
Tsypin Ilya <ilyaboy@list.ru>
2022-10-13 22:23:18 +0300

    ScanUtil first version

--- ScanUtil/Program.cs
+++ ScanUtil/Program.cs
@@ -1,3 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+namespace ScanUtil;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        var files = Directory.GetFiles(args[0]);
+
+        ushort jsCounter = 0;
+        ushort rmRfDetectsCounter = 0;
+        ushort runDllDetectsCounter = 0;
+        ushort errorsCounter = 0;
+
+        Parallel.ForEach(files, file =>
+        {
+            try
+            {
+                var lines = File.ReadLines(file);
+
+                switch (lines)
+                {
+                    case { } a when file.Contains(".js") && a.Contains("<script>evil_script()</script>"):
+                        jsCounter++;
+                        break;
+                    case { } b when b.Contains($"rm -rf {args[0]}"):
+                        rmRfDetectsCounter++;
+                        break;
+                    case { } c when c.Contains("Rundll32 sus.dll SusEntry"):
+                        runDllDetectsCounter++;
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                errorsCounter++;
+            }
+        });
+
+        stopWatch.Stop();
+
+        ScanModel scanModel = new()
+        {
+            ProcessedFiles = (ushort) files.Length,
+            JSDetects = jsCounter,
+            RmRfDetects = rmRfDetectsCounter,
+            RunDllDetects = runDllDetectsCounter,
+            Errors = errorsCounter,
+            ExecutionTime = stopWatch.Elapsed
+        };
+
+        Console.WriteLine(scanModel.ToString());
+    }
+}
---
+++ ScanUtil/ScanModel.cs
@@ -0,0 +1,21 @@
+﻿namespace ScanUtil;
+
+public class ScanModel
+{
+    public int ProcessedFiles { get; init; }
+    public int JSDetects { get; init; }
+    public int RmRfDetects { get; init; }
+    public int RunDllDetects { get; init; }
+    public int Errors { get; init; }
+    public TimeSpan ExecutionTime { get; init; }
+
+    public override string ToString() =>
+        "====== Scan result ======\n" +
+        $"Processed files: {ProcessedFiles}\n" +
+        $"JS detects: {JSDetects}\n" +
+        $"rm -rf detects: {RmRfDetects}\n" +
+        $"Rundll32 detects: {RunDllDetects}\n" +
+        $"Errors: {Errors}\n" +
+        $"Execution time: {ExecutionTime}\n" +
+        "=========================";
+}
