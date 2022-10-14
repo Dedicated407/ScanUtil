fac8e6f63517f8137f0b245c826a0fde02d9c347
Tsypin Ilya <ilyaboy@list.ru>
2022-10-14 17:16:23 +0300

    Add Repository and pattern ServiceLocator

---
+++ ServiceApplication/Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using ScanUtil.Models;
+
+namespace ServiceApplication.Infrastructure.Interfaces;
+
+public interface IRepository
+{
+    public Task Save(ScanResponseModel model);
+    public Task<ScanResponseModel> Find(Guid id);
+}
---
+++ ServiceApplication/Infrastructure/Repository.cs
@@ -0,0 +1,26 @@
+﻿using ScanUtil.Models;
+using ServiceApplication.Infrastructure.Interfaces;
+
+namespace ServiceApplication.Infrastructure;
+
+public class Repository : IRepository
+{
+    private readonly IList<ScanResponseModel> _responseModels;
+    
+    public Repository(IList<ScanResponseModel> responseModels)
+    {
+        _responseModels = responseModels;
+    }
+
+    public Task Save(ScanResponseModel model)
+    {
+        _responseModels.Add(model);
+        
+        return Task.CompletedTask;
+    }
+
+    public Task<ScanResponseModel> Find(Guid id)
+    {
+        return Task.FromResult(_responseModels.First(model => model.Id == id));
+    }
+}
